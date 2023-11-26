91b21d391452dd30f8a73c4d1aa5750f9b970d5f
3B032062 <3B032062@gm.student.ncut.edu.tw>
2023-11-26 22:28:12 +0800

    新增partials資料夾並新增navigation.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>@yield('title') | My Blog</title>
-</head>
 <body>
+@include('layouts.partials.navigation')
 <div class="container">
 @yield('content')
 </div>
 </body>
-</html>
