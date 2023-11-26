eeac0beeb659b53fd8287c7a65ea644e4cd69da0
3B032062 <3B032062@gm.student.ncut.edu.tw>
2023-11-26 22:33:00 +0800

    新增HelloController並建立hello資料家且修改Route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\HomeController;
 Route::get('home',[HomeController::class,'index']);
+
+use App\Http\Controllers\HelloController;
+Route::get('home/{name?}',[HelloController::class,'index'])->name('helloindex');
