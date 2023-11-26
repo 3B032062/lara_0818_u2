77b46934ec1a36f87441e223d5e968c9c0fb07d5
3B032062 <3B032062@gm.student.ncut.edu.tw>
2023-11-26 22:20:20 +0800

    修改route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('home', function () {
-    return view('home.index');
-});
+use App\Http\Controllers\HomeController;
+Route::get('home',[HomeController::class,'index']);
